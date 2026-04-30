671dc1e243c93a37185849403c788eabf2d9cd2e
chenbin <chenbin@tuzhanai.com>
2026-04-30 13:46:35 +0800

    update

--- src/components/SortableWrapper.tsx
+++ src/components/SortableWrapper.tsx
@@ -14,7 +14,7 @@ export const SortableWrapper: React.FC<Props> = ({ id, isSelected, onClick, chil
   const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({ id });
 
   const style = {
-    transform: CSS.Transform.toString(transform),
+    transform: CSS.Translate.toString(transform),
     transition,
     zIndex: isDragging ? 10 : 1,
     opacity: isDragging ? 0.5 : 1,
